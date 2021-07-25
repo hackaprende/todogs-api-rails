567a495982fc10925e332a1f133ca7101cb8ee12
Jesus Almaral <almaral17@gmail.com>
2021-07-25 14:55:09 -0500

    Update message in response

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,7 +1,7 @@
 module Response
     def json_response messages, is_success, data, status
         render json: {
-            messages: messages,
+            message: messages,
             is_success: is_success,
             data: data
         }, status: status
