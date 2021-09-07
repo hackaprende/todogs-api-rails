7c7560f4dd372e6872c69fb388c8e4bede076fbe
Jesus Valdez <hackaprende@gmail.com>
2021-09-06 22:25:29 -0500

    Add endpoint to update dogs

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -15,6 +15,16 @@ class Api::V1::DogsController < ApplicationController
         end
     end
 
+    def update
+        @dog = Dog.find(params[:id])
+
+        if @dog.update(dog_params)
+            json_response "Dog updated!", true, {dog: @dog}, :ok
+        else
+            json_response "Dog not found", false, {}, :ok
+        end
+    end
+
     private
     def dog_params
       params.require(:dog).permit(:dog_type, :height_female, :height_male, :image_url, :index, 
