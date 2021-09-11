de536e83279c36dcaf6923d57d5be1bbd3bea30c
Jesus Valdez <hackaprende@gmail.com>
2021-09-11 11:58:27 -0500

    Update registrations controller
    - Update params that need to be passed
    - Check if user already exists

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,22 +3,30 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
     # Sign up
     def create
+        if User.exists?(email: params[:email])
+            json_response "user_already_exists", false, { }, :ok
+            return
+        end
+
         user = User.new user_params
 
         if user.save
             json_response "Signed up successfully", true, { user: user }, :ok
         else
-            json_response "Someting wrong", false, { }, :unprocessable_entity
+            json_response "something_wrong", false, { }, :unprocessable_entity
         end
     end
 
     private
     def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
+        params.require(:email)
+        params.require(:password)
+        params.require(:password_confirmation)
+        params.permit(:email, :password, :password_confirmation)
     end
 
     def ensure_params_exist
-        return if params[:user].present?
+        return if params[:email].present? && params[:password].present? && params[:password_confirmation].present?
         json_response "Missing params", false, { }, :bad_request
     end
 end
